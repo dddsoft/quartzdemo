f0e80a86e173afa5f5c606dbbf0aaa86d5c7dfc2
dddsoft <oybek.allamov@gmail.com>
2025-03-26 21:11:37 +0500

    first commit

---
+++ index.php
@@ -0,0 +1,423 @@
+
+<!DOCTYPE html>
+<html lang="uz">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta name="csrf-token" content="6DZxvtVUcV4oaaD1vPvF0GlwKRmzbeD2uRIFbPOC">
+
+  <title>Uchrashuvlar</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+  <link href="https://mechauz.uz/logo/logo_mecha_uz.jpg" rel="icon">
+  <link href="https://mechauz.uz/logo/logo_mecha_uz.jpg" rel="apple-touch-icon">
+  <!-- Favicons -->
+  
+
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="https://mechauz.uz/assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="https://mechauz.uz/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="https://mechauz.uz/assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="https://mechauz.uz/assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="https://mechauz.uz/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: EstateAgency - v4.10.0
+  * Template URL: https://bootstrapmade.com/real-estate-agency-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+  <!-- Google tag (gtag.js) -->
+
+
+<body>
+
+  <!-- ======= Property Search Section ======= -->
+<div class="click-closed"></div>
+<!--/ Form Search Star /-->
+<div class="box-collapse">
+    <div class="title-box-d">
+        <h3 class="title-d">Qidirish</h3>
+    </div>
+    <span class="close-box-collapse right-boxed bi bi-x"></span>
+    <div class="box-collapse-wrap form">
+
+            <input type="hidden" name="_token" value="6DZxvtVUcV4oaaD1vPvF0GlwKRmzbeD2uRIFbPOC">            <div class="row">
+                <div class="col-md-12 mb-2">
+                    <div class="form-group">
+                        
+                        <input type="search" class="form-control form-control-lg form-control-a" placeholder="Kalit so‘zni kiriting" name="keyword" id="keyword">
+                        <input type="hidden" class="form-control form-control-lg form-control-a" placeholder="Kalit so'zni kiriting" name="lang" id="lang" value="uz">
+                    </div>
+                </div>
+
+
+
+            </div>
+
+        <div id="result" class="col-md-12 mt-3">
+        </div>
+    </div>
+
+</div>
+<!-- End Property Search Section -->
+
+<!-- ======= Header/Navbar ======= -->
+<nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+        <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarDefault" aria-controls="navbarDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span></span>
+            <span></span>
+            <span></span>
+        </button>
+        <a class="navbar-brand text-brand logo_text" href="/uz">
+                            <img src="https://mechauz.uz/system/logo/Q0XTrLv9EmHg6Qvtc7NUOl11CAsL1ZjH0tz3tVcR.png" alt="" class="logo" > 
+                    </a>
+
+        <div class="navbar-collapse collapse justify-content-center" id="navbarDefault">
+            <ul class="navbar-nav">
+
+                
+                <li class="nav-item">
+                    <a class="nav-link" href="/uz/about-us">Loyiha</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/news/all">Yangiliklar</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/activities/all">Faoliyatlar</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/meetings/all">Uchrashuvlar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/tenders/all">Tenderlar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/partners">Hamkorlar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/uz/contact">Bog‘lanish</a>
+                </li>
+                
+
+                <li class="nav-item">
+            <a class="nav-link " href="/uz/results">Natijalar</a>
+          </li>
+
+                
+                            <li class="nav-item dropdown ">
+            <a class="nav-link dropdown-toggle" href="https://mechauz.uz/uz/meetings/single/24" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >UZB <img src="https://mechauz.uz/img/uzb.png" alt=""  style="min-width:25px; max-width:35px;"></a>
+            <div class="dropdown-menu">
+                <a class="dropdown-item " href="https://mechauz.uz/en/meetings/single/24">ENG <img src="https://mechauz.uz/img/eng.png" alt="" style="min-width:25px; max-width:35px;"></a>
+            </div>
+        </li>
+    
+            </ul>
+        </div>
+
+        <button type="button" class="btn btn-b-n navbar-toggle-box navbar-toggle-box-collapse" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+            <i class="bi bi-search"></i>
+        </button>
+
+    </div>
+</nav>
+<!-- End Header/Navbar -->
+        <!-- ======= Property Search Section ======= -->
+  <!-- ======= Intro Section ======= -->
+  <div class="intro swiper position-relative mt-5 pt-5">
+
+    <div class="swiper-wrapper">
+
+      <div class="swiper-slide carousel-item-a intro-item bg-image" style="background-image:   url(https://mechauz.uz/banner-images/vF4GHp1LlJmHKco6Fgwdfen8ggM3qlbPEs8b8tW5.jpg)   ">
+        <div class="overlay overlay-a"></div>
+        <div class="intro-content display-table">
+          <div class="table-cell">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="intro-body">
+                    
+                    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+
+  <main id="main">
+
+    <!-- ======= NEWS Section ======= -->
+        <!-- ======= Intro Single ======= -->
+        <section class="mt-5">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12 col-lg-8">
+                  <div class="title-single-box news_all">
+                    <h1 class="title-single ms-3">Uchrashuvlar</h1>
+                    
+                  </div>
+                </div>
+                <div class="col-md-12 col-lg-4">
+                  <nav aria-label="breadcrumb" class="breadcrumb-box d-flex justify-content-lg-end">
+                    <ol class="breadcrumb">
+                      <li class="breadcrumb-item">
+                        <a href="/uz">Asosiy</a>
+                      </li>
+                      <li class="breadcrumb-item">
+                        <a href="/uz/meetings/all">Barcha uchrashuvlar</a>
+                      </li>
+                      <li class="breadcrumb-item active" aria-current="page">
+                        Uchrashuvlar                      </li>
+                    </ol>
+                  </nav>
+                </div>
+              </div>
+            </div>
+          </section>
+          <!-- End Intro Single -->
+    <!-- End Services Section -->
+
+        <!-- ======= Agent Single ======= -->
+                <section class="agent-single">
+          <div class="container">
+            <div class="row">
+              <div class="col-sm-12 mb-3">
+                <div class="row d-flex justify-content-center">
+                  <div class="col-md-8 section-md-t3">
+                    <div class="agent-info-box">
+                      <div class="agent-title">
+                        <div class="title-box-d">
+                          <h3 class="title-d fs-1">Loyihaning yakuniy uchrashuvi</h3>
+                          <div class="pb-3 d-flex justify-content-start">
+                            <span class="text-muted text-uppercase"><span class="bi bi-calendar-fill text-success fw-bold"></span> 
+                             08-10-2023 
+                             <span class="bi bi-person-fill text-success fw-bold ms-2"></span> 
+                                                            <span class="text-capitalize">Muallif:</span> RAXMATILLAYEV JAVLONBEK                                  </span>
+                          </div>
+                        </div>
+                      </div>
+                      <div class="agent-content mb-3">                          
+                        <p class="content-d color-text-a w-100">
+                          <p style="text-align: center;"><img src="../../../../../storage/files/NM2Xwm8OWE17Cts8AdWEGBUluz4w7b7Qi7x9f5mC.jpg" alt="" width="900" height="509"></p>
+<hr>
+<p><strong>Ushrashuv ilovalari</strong></p>
+<ul>
+<li style="list-style-type: none;">
+<ul>
+<li><a href="../../../../../storage/files/MxV7mLwkfbQrsnLI7uBfmCdu0IjetMRTeVjcTu4Y.pdf" target="_blank" rel="noopener">Yakuniy uchrashuv kun tartibi_pdf</a></li>
+<li><a href="../../../../../storage/files/shMeV8Qffo93yCFoeSiodQA6CqFmQozIn7shm89F.pdf" target="_blank" rel="noopener">Yakuniy uchrashuv bayonnomasi_pdf</a></li>
+<li><a href="../../../../../storage/files/o3JVx3hqjY8BYcaIEB41dEmkgkVdBIqkG3edKvkS.pdf" target="_blank" rel="noopener">Konferensiya axborot xati_pdf</a></li>
+<li><a href="../../../../../storage/files/tjJ1qQRxatuSeOzivLEv70c4lDwACWGoyC8Yd9wB.pdf" target="_blank" rel="noopener">Konferensiya bukleti_pdf</a></li>
+<li><a href="../../../../../storage/files/2twuonQkPXv86LzJlNEFBezcEWUm9J5to7q8HEcM.pdf" target="_blank" rel="noopener">Uchrashuv sertifikatlari_pdf</a></li>
+<li><a href="../../../../../storage/files/eM82mQ4U78YpNHb83CglIbGLjt1V3ITpS7lU30K1.pdf" target="_blank" rel="noopener">Uchrashuv bannneri_pdf</a></li>
+</ul>
+</li>
+</ul>
+<hr>
+<p><strong>Loyiha uchrashuvi taqdimotlari</strong></p>
+<ul>
+<li style="list-style-type: none;">
+<ul>
+<li><a href="../../../../../storage/files/miGbOkIRaAS6oX9kkxBUecztUnLIe1FfDHVBTQBP.pdf" target="_blank" rel="noopener">External_Evaluation_Presentation_pdf</a></li>
+<li><a href="../../../../../storage/files/fpnENPU0giczDNbrRtpMlOty2kSSQe0NA12Ekb5S.pdf" target="_blank" rel="noopener">Final meeting_AndMI_pdf</a></li>
+<li><a href="../../../../../storage/files/cz45IgBunwrH5lmxP9AUEbuhLVZBxVMiefge1Rp2.pdf" target="_blank" rel="noopener">Final meeting_F9_FerPI_pdf</a></li>
+<li><a href="../../../../../storage/files/EJl33sTWw4VCc6LkIYJjb7pDWNAmacBcltv7RK20.pdf" target="_blank" rel="noopener">Final meeting_MechaUz_KEEI_pdf</a></li>
+<li><a href="../../../../../storage/files/7RO0q3YaFVmWEql3sPjCW4plfLDB92ubsvl8fkFy.pdf" target="_blank" rel="noopener">Final meeting_P8_TTPU_pdf</a></li>
+<li><a href="../../../../../storage/files/5W9VYLiuxKzcdYUW3GczizOBWOnIG7Nw62kd8RPe.pdf" target="_blank" rel="noopener">Final meeting_TSTU_pdf</a></li>
+<li><a href="../../../../../storage/files/eWJPcPRqTyTEmum3vnmg9gi8beNWSgsUJChhc2Bv.pdf" target="_blank" rel="noopener">Final meeting_TUIT_01.10.2023_pdf</a></li>
+<li><a href="../../../../../storage/files/RwuqbOTpX0H8E1N6KpBXdMm4LvkK68WNVH95oAw6.pdf" target="_blank" rel="noopener">MechaUz_Establishment_of_I_LABs,_Training_Centre_and_Society_pdf</a></li>
+<li><a href="../../../../../storage/files/86rm8lVR48O4Dfl2LuSaPg8XyVFJlJBmZxh2B9Hs.pdf" target="_blank" rel="noopener">TSTU final presentation_pdf</a></li>
+<li><a href="../../../../../storage/files/VBvECMN5vkq2BDbCWy42S09DWlLGrgpgYIe3wwHn.pdf" target="_blank" rel="noopener">MechaUz_Brief Summary of project activities_pdf</a></li>
+</ul>
+</li>
+</ul>
+                        </p>
+                      </div>                        
+                    </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+                    <!-- End Agent Single -->
+
+  </main>
+  <!-- End #main -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- ======= Footer ======= -->
+      <section class="section-footer" id="contact">
+    <noscript><div><img src="https://mc.yandex.ru/watch/92572681" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <div class="widget-a">
+            <div class="w-header-a mb-3">
+              <a class="navbar-brand text-brand logo_text" href="/uz"> 
+                                <img src="https://mechauz.uz/system/logo/Q0XTrLv9EmHg6Qvtc7NUOl11CAsL1ZjH0tz3tVcR.png" alt="" class="logo" > 
+                          </a>
+            </div>
+            <div class="w-body-a">
+              <p class="w-text-a color-text-a">
+                Ushbu loyiha Yevropa Komissiyasi ko&#039;magida moliyalashtirildi. Bu veb-sayt faqat mualliflarning fikrlarini aks ettiradi va Komissiya undagi ma&#039;lumotlardan foydalanish uchun javobgarlikni o&#039;z zimmasiga olmaydi. Oliy ta&#039;lim sohasida salohiyatni oshirish loyihalari. Loyiha raqami: 609564-EPP-1-2019-1-EL-EPPKA2-CBHE-JP
+              </p>
+            </div>
+            <div class="w-footer-a">
+              <ul class="list-unstyled">
+                <li class="color-a">
+                  <span class="color-text-a">Telefon raqami.</span> +306976636616
+                </li>
+                <li class="color-a">
+                  <span class="color-text-a">Eektron po‘chta manzili.</span> <a  href="mailto:ihu.mechan@gmail.com">ihu.mechan@gmail.com</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">Veb-sayt havolalari</h3>
+            </div>
+            <div class="w-body-a">
+              <ul class="list-unstyled">
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/about-us">Loyiha</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/news/all">Yangiliklar</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/activities/all">Faoliyatlar</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/meetings/all">Uchrashuvlar</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/tenders/all">Tenderlar</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> <a href="/uz/partners">Hamkorlar</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+                <div class="col-sm-12 col-md-4 section-md-t3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h3 class="w-title-a text-brand">Bizning veb-saytlarimiz</h3>
+            </div>
+            <div class="w-body-a">
+              <ul class="list-unstyled">
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> Loyiha web saytining avvalgi shakli <br> <a href="https://www.mecha.uz" target="_blank" class="ms-4">(www.mecha.uz)</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> Loyihaning onlayn ta'lim platformasi <br> <a href="https://www.e-mechauz.uz" target="_blank" class="ms-4">(www.e-mechauz.uz)</a>
+                </li>
+                <li class="item-list-a">
+                  <i class="bi bi-chevron-right"></i> Mexatronika jamiyati veb sayti <br> <a href="https://www.society.mechauz.uz" target="_blank" class="ms-4">(society.mechauz.uz)</a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          
+          <div class="socials-a">
+            <ul class="list-inline">
+             
+              <li class="list-inline-item">
+                <a href="https://www.youtube.com/channel/UCbCy3vI_m2D3qW-pCFHVmTQ">
+                  <i class="bi bi-youtube" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://www.facebook.com/Mechauz-100164515075994">
+                  <i class="bi bi-facebook" aria-hidden="true"></i>
+                </a>
+              </li>              
+              <li class="list-inline-item">
+                <a href="https://www.instagram.com/mechauz?r=nametag">
+                  <i class="bi bi-instagram" aria-hidden="true"></i>
+                </a>
+              </li>
+              <li class="list-inline-item">
+                <a href="https://t.me/MechaUZ_Dissemination">
+                  <i class="bi bi-telegram" aria-hidden="true"></i>
+                </a>
+              </li>             
+            </ul>
+          </div>
+          <div class="copyright-footer">
+            <p class="copyright color-text-a">
+              &copy; Copyright
+              <span class="color-a"><a href="https://mecha.uz/client/indexuz.php" target="_blank">MechaUz</a></span> All Rights Reserved.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End  Footer -->  
+   
+
+  
+
+  <script src="https://mechauz.uz/js/jquery.min.js"></script>
+  <script src="https://mechauz.uz/js/script.js"></script>
+   <script>
+    $(document).ready(function (){
+        $('#keyword').on('keyup',function (){
+            var keyword = $(this).val();
+            var lang = $('#lang').val();
+            $.ajax({
+                url:"/uz/search",
+                type: "GET",
+                data:{'keyword' :keyword,'lang':lang},
+                success:function (news){
+                    // $('#resultList').html(news);
+                    $('#result').html(news);
+        }
+    })
+})
+});
+</script>
+  
+        <!-- Vendor JS Files -->
+  <script src="https://mechauz.uz/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="https://mechauz.uz/assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="https://mechauz.uz/assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="https://mechauz.uz/assets/js/main.js"></script>
+	
+</body>
+
+</html>
